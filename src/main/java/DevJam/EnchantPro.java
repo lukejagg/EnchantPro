985407b9b436bee9e58a6af13e3d9010bc93f5be
Lucas Jagg <lukejagg@gmail.com>
2021-05-23 16:38:52 -0500

    Added testing command processor

--- src/main/java/DevJam/EnchantPro.java
+++ src/main/java/DevJam/EnchantPro.java
@@ -1,8 +1,21 @@
 package DevJam;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EnchantPro extends JavaPlugin {
+    public static EnchantPro Instance;
+
+    public EnchantPro() {
+        Instance = this;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandlabel, String[] args) {
+        return CommandProcessor.onCommand(sender, command, commandlabel, args);
+    }
+
     @Override
     public void onEnable() {
         getLogger().info("onEnable is called!");
@@ -11,4 +24,6 @@ public class EnchantPro extends JavaPlugin {
     public void onDisable() {
         getLogger().info("onDisable is called!");
     }
+
+
 }
