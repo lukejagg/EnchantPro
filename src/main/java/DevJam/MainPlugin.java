c8c68c7a6f1f5079a5d28a6119100c087f020c9c
Lucas Jagg <lukejagg@gmail.com>
2021-05-23 15:10:23 -0500

    Initial commit

---
+++ src/main/java/DevJam/MainPlugin.java
@@ -0,0 +1,14 @@
+package DevJam;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
