6389c9d979daf9610a6867ad82759b3e0a5fff96
Lucas Jagg <lukejagg@gmail.com>
2021-05-23 15:27:23 -0500

    Renamed main file

--- src/main/java/DevJam/EnchantPro.java
+++ src/main/java/DevJam/EnchantPro.java
@@ -2,7 +2,7 @@ package DevJam;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainPlugin extends JavaPlugin {
+public class EnchantPro extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("onEnable is called!");
